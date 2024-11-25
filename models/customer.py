8027379b9b5c95a9489e990cc93dc12c64588181
sarayu <sarayurk96@gmail.com>
2024-11-25 16:02:44 +0530

    my customer file committed

---
+++ models/customer.py
@@ -0,0 +1,7 @@
+class Customer:
+    def __init__(self,customerName,age):
+        self.customerName =customerName
+        self.age = age
+    def display(self):
+        print(f"the customer name is {self.customerName}")
+        
