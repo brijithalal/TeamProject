8e0c9e98687d9e86b3ba05a53aebb2dfa56fa003
brijitha <brijithalal36@gmail.com>
2024-11-25 15:40:36 +0530

    created a project

---
+++ models/employee.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self,employeeName,age):
+        self.employeeName =employeeName
+        self.age = age
+    def display(self):
+        print(f"the employee name is {self.employeeName}")
+        
