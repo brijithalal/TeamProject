b94206e99bf86fbfae0668d2575bdff8f8187fa7
binu <binu.christin@faithinfotech.in>
2024-11-25 16:02:45 +0530

    my admin class added

---
+++ models/admin.py
@@ -0,0 +1,7 @@
+class admin:
+    def __init__(self,adminName,age):
+        self.adminName =adminName
+        self.age = age
+    def display(self):
+        print(f"the adminName is {self.adminName}")
+        
